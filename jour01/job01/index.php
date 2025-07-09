85387f3610d90d570db53c5022d99fb914db497b
gabriel-viallard-fortier <gabriel.viallard-fortier@laplateforme.io>
2025-07-09 15:35:22 +0200

    job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$varString = "LaPlateforme";
+echo $varString;
+
+$varString2 = "Vive";
+$varstring3 = "!";
+
+echo $varString2 . " " . $varString . " " . $varstring3;
+
+$val = 6;
+echo $val;
+
+$val = $val + 4;
+echo $val;
+
+$myBool = true;
+echo $myBool;
+
+$myBool = false;
+echo $myBool;
+
+?>
