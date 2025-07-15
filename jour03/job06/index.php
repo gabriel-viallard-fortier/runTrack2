2f6189366172aad258845266fe5a75df854b3aa3
gabriel-viallard-fortier <gabriel.viallard-fortier@laplateforme.io>
2025-07-15 13:15:45 +0200

    php terminé

--- jour03/job06/index.php
+++ jour03/job06/index.php
@@ -2,7 +2,9 @@
 
 $str = "Les choses que l'on possède finissent par nous posséder.";
 
-// On veut retourner à l'envers cette chaine de caracteres 
+// On veut retourner à l'envers cette chaine de caracteres
+
+
 $tableau = mb_str_split($str);
 
 for ($i = strlen($str); $i >= 0; $i--)
