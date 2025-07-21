9924f6f4a363821790c1c6dd9f2abc5beaa9621e
gabriel-viallard-fortier <gabriel.viallard-fortier@laplateforme.io>
2025-07-21 15:13:55 +0200

    correction isset j03 j06

--- jour03/job06/index.php
+++ jour03/job06/index.php
@@ -1,21 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
 
-$str = "Les choses que l'on possède finissent par nous posséder.";
+    $str = "Les choses que l'on possède finissent par nous posséder.";
 
-// On veut retourner à l'envers cette chaine de caracteres
+    // On veut retourner à l'envers cette chaine de caracteres
 
-$longueurChaine = 0;
+    $longueurChaine = 0;
 
-for ($i = 0; isset($str[$i]); $i++) {
-        $longueurChaine = $i;
-}
-
-
-for ($i = $longueurChaine ; $i >= 0 ; $i --)
-{
-    echo $str[$i];
-}
+    for ($i = 0; isset($str[$i]); $i++) {
+            $longueurChaine = $i;
+    }
 
 
+    for ($i = $longueurChaine ; $i >= 0 ; $i --)
+    {
+        echo $str[$i];
+    }
 
-?>
+    ?>
+</body>
+</html>
+
+
+
+
+
