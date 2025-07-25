e5f23042912db9a083ca91bd0c1ca2569ce678e2
gabriel-viallard-fortier <gabriel.viallard-fortier@laplateforme.io>
2025-07-25 09:00:13 +0200

    commentaires

---
+++ jour08/job08/index.php
@@ -0,0 +1,47 @@
+<?php
+    if (isset($_POST['reset'])) {
+    // si on presse reset
+
+        setcookie("nombreVisites", "");
+        //on reinitialise le cookie
+        
+        $nombreVisites = 0;
+
+        //on recharge la page
+        header("Location: " . $_SERVER['PHP_SELF']);
+        
+        // et on quitte le script
+        exit;
+    }
+    else {
+    // sinon
+    //  si le cookie n'est pas défini
+        if (!isset ($_COOKIE["nombreVisites"])) {
+            // on initialise le comteur
+            $nombreVisites = 1;
+        }
+        // si le cookie est défini, on l'incrémente
+        else {
+            $nombreVisites = $_COOKIE["nombreVisites"] + 1;
+        }
+
+        // on défini le cookie et on affiche le nombre de visites
+        setcookie ("nombreVisites", $nombreVisites);
+        echo "Nombre de visites : " . $nombreVisites . "<br>";
+    }
+
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+        <button type="submit" name="reset">Reset</button>
+    </form>
+</body>
+</html>
